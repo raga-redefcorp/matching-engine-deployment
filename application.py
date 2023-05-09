d22cfeeeea5d22d908bce8b72d0f9d304120c3c7
raga-redefcorp <raga@redefcorp.in>
2023-05-09 15:44:36 +0530

    :pacakge: fixed config issue

---
+++ application.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from uvicorn import run
+from dotenv import load_dotenv
+import os
+from app.routers import resume
+
+# Load env variables
+load_dotenv()
+
+application = FastAPI()
+
+app = application
+
+origins = ["*"]
+methods = ["*"]
+headers = ["*"]
+
+app.add_middleware(
+    CORSMiddleware, 
+    allow_origins = origins,
+    allow_credentials = True,
+    allow_methods = methods,
+    allow_headers = headers    
+)
+
+# Adds the router to the main app instance
+app.include_router(resume.router)
+    
+if __name__ == "__main__":
+	port = int(os.environ.get('PORT', 5000))
+	run(app, host=os.environ.get('HOST', "0.0.0.0"), port=port)
